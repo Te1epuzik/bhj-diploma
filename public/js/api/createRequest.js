4aa449feb22ff861392781be890ca8402c9396b4
Neto test <test@neto.com>
2024-05-12 00:51:14 +0300

    Func createRequest

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,33 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
+const createRequest = (options = {
+	method: 'GET',
+	url,
+	data: {
+		mail,
+		password
+	}
+}) => {
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		xhr.open(options.method, options.url, true);
+		xhr.responseType = 'json';
+		
+		xhr.addEventListener('load', event => {
+			event.preventDefault();
 
+			if (xhr.status < 400) {
+				resolve(xhr.response);
+			} else {
+				reject(xhr.response);
+			}
+		});
+
+		if (options.data) {
+			xhr.send(JSON.stringify(options.data));
+		} else {
+			xhr.send();
+		}
+	});
 };
